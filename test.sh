3421dbab51e03539a0c5084c612bb73e2a9c4d19
Bradley McKay <bradley.mckay@sandtechsolutions.com>
2024-03-21 14:32:33 -0600

    Fixed test.sh syntax

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-!#/bin/bash
+#!/bin/bash
 echo "Running sbom-puller against atlassian/jira-core:9.13-jdk11"
 python3 sbom-puller.py -i atlassian/jira-core:9.13-jdk11
-echo "Check these docker images to ensure atlassian/jira-core:9.13-jdk11 was removed:"
-docker images
+echo "Check to ensure atlassian/jira-core:9.13-jdk11 was removed:"
+docker images | grep 9.13-jdk11
