42ff463cd916fad27aa52d2f3c122a040da1293b
Bradley McKay <bradley.mckay@sandtechsolutions.com>
2024-03-21 14:29:17 -0600

    fixed command shell issue to echo result

--- sbom-puller.py
+++ sbom-puller.py
@@ -39,7 +39,7 @@ def pull_and_run_image(images_to_assess):
         print(f'Pulling, running, and extracting SBOM from image: {image}')
         pulled_image = client.images.pull(image)
         container = client.containers.run(image, detach=True)
-        exec_result = container.exec_run('sh -c "SBOM_LOCATION=$(find / -iname *sbom* 2>/dev/null)"')
+        exec_result = container.exec_run('sh -c "SBOM_LOCATION=$(find / -iname *sbom* 2>/dev/null); echo $SBOM_LOCATION"')
         print(exec_result.output.decode('utf-8'))
 
         # Kill and remove the container then remove the image
