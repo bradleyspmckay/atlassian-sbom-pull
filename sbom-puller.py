6c887017c6d06dae009c6176753b7b7bf9e45733
Bradley McKay <bradley.mckay@sandtechsolutions.com>
2024-03-21 13:08:22 -0600

    App created created to test docker operations.

--- sbom-puller.py
+++ sbom-puller.py
@@ -2,33 +2,44 @@ import docker
 import argparse
 import os
 
-parser = argparse.ArgumentParser(prog='SBOM-Puller',
-                                 description='This program will pull an Atlassian image and then extract its SBOM if one is available.')
+parser = argparse.ArgumentParser(prog='Atlassian-SBOM-Puller',
+                                 description='This program will pull an Atlassian image and then extract its SBOM if one is available.',
+                                 epilog='Please see https://github.com/bradleyspmckay/atlassian-sbom-pull for further documentation')
 
-def pull_and_run_image(registry, name, tag, command=None):
+parser.add_argument('-i', '--image', dest='image', required=False, help='Full image path: registry.tld/repository/path/image:tag')
+parser.add_argument('-il', '--image-list', dest='image_list', required=False, help='List of full paths to all images to be assessed')
+parser.add_argument('-o', '--ouput', dest='output_directory', required=False, help='Name of output directory if not current directory')
+#parser.add_argument()
+
+args = parser.parse_args()
+
+# Check if image path or list of image paths was provided and exit if not
+if not args.image and not args.image_list:
+    print('Please provide an image path or file containing a list of image paths')
+    exit()
+
+images_to_assess = []
+if args.image:
+    images_to_assess.append(args.image)
+if args.image_list:
+    with open(args.image_list, 'r') as file:
+        for line in file:
+            images_to_assess.append(line)
+if images_to_assess == []:
+    print('Failed to prime image or list of images, please check your input to -i or -il')
+    exit()
+
+def pull_and_run_image(images_to_assess):
     client = docker.from_env()
-    
-    image = f"{registry}/{name}:{tag}"
-    
-    print(f"Pulling image: {image}")
-    client.images.pull(image)
-    
-    print(f"Running container from image: {image}")
-    container = client.containers.run(image, detach=True)
-    
-    if command:
-        print(f"Executing command '{command}' within container {container.id}")
-        exec_result = container.exec_run(command)
-        print("Command output:")
+    for image in images_to_assess:
+        print(f'Pulling, running, and extracting SBOM from image: {image}')
+        client.images.pull(image)
+        container = client.containers.run(image, detach=True)
+        exec_result = container.exec_run('SBOM_LOCATION=$(find / -iname sbom 2>/dev/null)')
         print(exec_result.output.decode('utf-8'))
-    
-    container.stop()
-    container.remove()
+        container.stop()
+        container.remove()
 
 if __name__ == "__main__":
-    registry = input("Enter image registry (e.g., docker.io): ")
-    name = input("Enter image name: ")
-    tag = input("Enter image tag: ")
-    command = input("Enter command to execute within the container (leave empty to skip): ")
     
-    pull_and_run_image(registry, name, tag, command)
+    pull_and_run_image(images_to_assess)
